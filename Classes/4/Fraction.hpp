9659ec472c3d7a4afb43dd92f1ddbf9d7f8fd087
javabird25 <shockck84@gmail.com>
2021-04-14 14:04:26 +0500

    Начал переделывать дроби

--- Classes/4/Fraction.hpp
+++ Classes/4/Fraction.hpp
@@ -3,23 +3,75 @@
 #include <iostream>
 #include <string>
 
-class Fraction {
+template<typename T>
+static T gcd(T a, T b) {
+    while (a && b) {
+        if (a > b)
+            a %= b;
+        else
+            b %= a;
+    }
+    return a + b;
+}
+
+class ZeroDivisionException : std::domain_error {
 public:
-    Fraction(int, int);
-
-    int numerator() const { return _numerator; }
-    int denominator() const { return _denominator; };
-    void reduce();
-    Fraction flipped() const;
-    Fraction operator-() const;
-    std::string to_string() const;
-
-private:
-    int _numerator = 0, _denominator = 1;
+    ZeroDivisionException(const char *msg) : std::domain_error(msg) {}
 };
 
-Fraction operator+(const Fraction &, const Fraction &);
-Fraction operator-(const Fraction &, const Fraction &);
-Fraction operator*(const Fraction &, const Fraction &);
-Fraction operator/(const Fraction &, const Fraction &);
-std::ostream &operator<<(std::ostream, const Fraction &);
+template<typename T>
+class Fraction {
+public:
+    Fraction(T numerator, T denominator) : _numerator(numerator), _denominator(denominator) {
+        if (!_denominator)
+            throw ZeroDivisionException("denominator is 0");
+    }
+
+    T numerator() const { return _numerator; }
+    T denominator() const { return _denominator; };
+
+    void reduce() {
+        T d = gcd(_numerator, _denominator);
+        _numerator /= d;
+        _denominator /= d;
+    }
+
+    std::string to_string() const {
+        std::stringstream ss;
+        ss << _numerator;
+        if (_numerator)
+            ss << '/' << _denominator;
+        return ss.str();
+    }
+
+private:
+    T _numerator, _denominator;
+};
+
+Fraction operator+(const Fraction &a, const Fraction &b) {
+    Fraction result(
+        a.nominator() * b.denominator() + b.nominator() * a.denominator(),
+        a.denominator() * b.denominator()
+    );
+    result.reduce();
+    return result;
+}
+
+Fraction operator-(const Fraction &a, const Fraction &b) {
+    return operator+(-b);
+}
+
+Fraction operator*(const Fraction &a, const Fraction &b) {
+    Fraction result(a.numerator() * b.numerator(), a.denominator() * b.denominator());
+    result.reduce();
+    return result;
+}
+
+Fraction operator/(const Fraction &a, const Fraction &b) {
+    return operator*(Fraction(_denominator, _numerator));
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream s, const Fraction<T> &frac) {
+    return s << frac.to_string();
+}
--- Classes/4/gtest.cpp
+++ Classes/4/gtest.cpp
@@ -4,69 +4,64 @@
 
 #include "Fraction.hpp"
 
-void EXPECT_FRAC_EQ(Fraction &frac, int num, int denom) {
+template <typename T>
+void EXPECT_FRAC_EQ(Fraction<T> &frac, T num, T denom) {
     EXPECT_EQ(frac.numerator(), num);
     EXPECT_EQ(frac.denominator(), denom);
 }
 
 TEST(Fraction, reduce) {
-    Fraction f1(10, 10);
+    Fraction<int> f1(10, 10);
     f1.reduce();
     EXPECT_FRAC_EQ(f1, 1, 1);
 
-    Fraction f2(10, 5);
+    Fraction<int> f2(10, 5);
     f2.reduce();
     EXPECT_FRAC_EQ(f2, 2, 1);
 }
 
 TEST(Fraction, reduce_already_reduced) {
-    Fraction f1(5, 4);
+    Fraction<int> f1(5, 4);
     f1.reduce();
     EXPECT_FRAC_EQ(f1, 5, 4);
 }
 
-TEST(Fraction, reduce_zero_denominator) {
-    Fraction f(1, 0);
-    EXPECT_THROW(f.reduce(), std::domain_error);
+TEST(Fraction, zero_denominator) {
+    EXPECT_THROW(Fraction<int> f(1, 0), ZeroDivisionException);
 }
 
 TEST(Fraction, flipped) {
-    auto f = Fraction(1, 3).flipped();
+    auto f = Fraction<int>(1, 3).flipped();
     EXPECT_FRAC_EQ(f, 3, 1);
 }
 
-TEST(Fraction, negate) {
-    auto f = -Fraction(1, 1);
-    EXPECT_FRAC_EQ(f, -1, 1);
-}
-
 TEST(Fraction, add) {
-    auto f = Fraction(2, 3) + Fraction(2, 6);
+    auto f = Fraction<int>(2, 3) + Fraction<int>(2, 6);
     EXPECT_FRAC_EQ(f, 1, 1);
 }
 
 TEST(Fraction, subtract) {
-    auto f = Fraction(3, 3) - Fraction(1, 5);
+    auto f = Fraction<int>(3, 3) - Fraction<int>(1, 5);
     EXPECT_FRAC_EQ(f, 4, 5);
 }
 
 TEST(Fraction, multiply) {
-    auto f = Fraction(1, 10) * Fraction(10, 1);
+    auto f = Fraction<int>(1, 10) * Fraction<int>(10, 1);
     EXPECT_FRAC_EQ(f, 1, 1);
 }
 
 TEST(Fraction, divide) {
-    auto f = Fraction(1, 10) / Fraction(10, 1);
+    auto f = Fraction<int>(1, 10) / Fraction<int>(10, 1);
     EXPECT_FRAC_EQ(f, 1, 100);
 }
 
 TEST(Fraction, to_string) {
-    Fraction f(-1, 3);
+    Fraction<int> f(-1, 3);
     EXPECT_EQ(f.to_string(), "-1/3");
 }
 
 TEST(Fraction, to_string_zero) {
-    Fraction f(-0, 3);
+    Fraction<int> f(-0, 3);
     EXPECT_EQ(f.to_string(), "0");
 }
 
