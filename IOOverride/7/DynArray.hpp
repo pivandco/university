3607a303791d2deea8e64e24d9d7e9a10bb248bd
javabird25 <shockck84@gmail.com>
2021-05-06 13:31:57 +0500

    Сделал IOOverride/7

---
+++ IOOverride/7/DynArray.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <iostream>
+
+template<typename T>
+struct DynArray {
+    size_t size = 0;
+    T *array = nullptr;
+
+    ~DynArray() {
+        free(array);
+    }
+
+    void resize(size_t new_size) {
+        size = new_size;
+        array = (T *)realloc(array, new_size * sizeof(T));
+    }
+
+    void append(T obj) {
+        resize(size + 1);
+        array[size - 1] = obj;
+    }
+
+    T &operator[](size_t index) {
+        return array[index];
+    }
+};
+
+template<typename T>
+std::ostream &operator<<(std::ostream &out, const DynArray<T> &arr) {
+    out.write(reinterpret_cast<const char *>(&arr.size), sizeof(arr.size));
+    return out.write(reinterpret_cast<char *>(arr.array), arr.size * sizeof(T));
+}
+
+template<typename T>
+std::istream &operator>>(std::istream &in, DynArray<T> &arr) {
+    size_t size;
+    in.read(reinterpret_cast<char *>(&size), sizeof(size));
+    arr.resize(size);
+    return in.read(reinterpret_cast<char *>(arr.array), arr.size * sizeof(T));
+}
