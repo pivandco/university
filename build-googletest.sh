b7f40f62ef136353768d04c56b56565311115e4c
javabird25 <shockck84@gmail.com>
2021-05-12 12:33:32 +0500

    Исправил сборку под форточками

--- build-googletest.sh
+++ build-googletest.sh
@@ -2,7 +2,8 @@
 
 if [ "$(uname -o)" = "Msys" ]; then
     cmake -G 'Unix Makefiles' -B googletest-build '-DCMAKE_MAKE_PROGRAM=C:\Users\utkin.n\mingw\bin\make.exe' googletest
+    /bin/make -C googletest-build
 else
     cmake -G 'Unix Makefiles' -B googletest-build googletest
+    make -C googletest-build
 fi
-make -C googletest-build
