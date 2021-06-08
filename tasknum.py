b540d9bfe06ff8e3e3f5e19da83b37ba457e69d0
javabird25 <shockck84@gmail.com>
2021-06-08 10:47:30 +0500

    Апгрейд tasknum.py

--- tasknum.py
+++ tasknum.py
@@ -1,4 +1,7 @@
-variant = 23
+#!/usr/bin/env python3
+variant = int(input("Variant: "))
 tasks = int(input("Tasks: "))
-print(variant % tasks, (variant + 12) % tasks)
+print("+12:", variant % tasks, (variant + 12) % tasks)
+print("+13:", variant % tasks, (variant + 13) % tasks)
+print("Press Enter to exit...")
 input()
